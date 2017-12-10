3ee4b5d8e1730e0631010fafbf65170b4a15d317
Rafael Kuniyoshi <rafael.kuniyoshi12@gmail.com>
2017-12-10 19:44:54 -0200

    Creating model and visualising predictions

--- SimpleLinearRegressionTestRafael.py
+++ SimpleLinearRegressionTestRafael.py
@@ -16,6 +16,29 @@ y = dataset.iloc[:, 1].values
 
 #Splitting the dataset into the Training set and the Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, ramdom_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#Fitting simple linear regression into the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting Test set results
+y_pred = regressor.predict(X_test)
+
+#Visualising training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
 
 
